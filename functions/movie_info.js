ee66b61adc1b969413d66c19394b1d9cdab6ac34
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2021-07-21 16:51:50 +0900

    ・映画情報取得関数を追加する。

---
+++ functions/movie_info.js
@@ -0,0 +1,76 @@
+import { ResponseHeader } from '../data/response_header';
+import { movieInfo } from '../data/movie_info';
+import { area, prefecture, theater } from '../data/theater';
+
+export const handler = async (event, context) => {
+
+  var theaters = [];
+  if (movieInfo.theaterId && movieInfo.theaterId.length > 0) {
+    movieInfo.theaterId.forEach(function (theaterId) {
+      if (theater[theaterId - 1]) {
+        var theaterCopy = { ...theater[theaterId - 1] }
+        theaterCopy.id = theaterId;
+        theaters.push(theaterCopy)
+      }
+    });
+  }
+
+  theaters.sort((a, b) => {
+    if ((a.area > b.area) ||
+    (a.area === b.area && a.prefecture > b.prefecture) ||
+    (a.area === b.area && a.prefecture === b.prefecture && a.id > b.id)) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+
+  var theatersByAreaPrefecture = [];
+  var preAreaId = null;
+  var prePrefectureId = null;
+  var tmpTheatersByAreaPrefecture = null;
+  var prefectureTheaters = null;
+  theaters.forEach(function (theater) {
+    const areaId = theater.area;
+    const prefectureId = theater.prefecture;
+    if (!preAreaId || preAreaId !== areaId) {
+      if (tmpTheatersByAreaPrefecture) {
+        theatersByAreaPrefecture.push(tmpTheatersByAreaPrefecture);
+      }
+      tmpTheatersByAreaPrefecture = {
+        area: area[areaId],
+        prefectures: []
+      }
+      prefectureTheaters = [];
+    } else if (prePrefectureId !== prefectureId) {
+      tmpTheatersByAreaPrefecture.prefectures.push({
+        prefecture: prefecture[prePrefectureId],
+        theater: prefectureTheaters
+      });
+      prefectureTheaters = [];
+    }
+    prefectureTheaters.push(theater);
+    preAreaId = areaId;
+    prePrefectureId = prefectureId;
+  });
+  if (tmpTheatersByAreaPrefecture) {
+    if (prefectureTheaters) {
+      tmpTheatersByAreaPrefecture.prefectures.push({
+        prefecture: prefecture[prePrefectureId],
+        theater: prefectureTheaters
+      });
+    }
+    theatersByAreaPrefecture.push(tmpTheatersByAreaPrefecture);
+  }
+
+  const movieInfoCopy = { ...movieInfo };
+  movieInfoCopy.theater = theatersByAreaPrefecture;
+  delete movieInfoCopy.theaterId;
+
+  return {
+    statusCode: 200,
+    headers: ResponseHeader,
+    body: JSON.stringify(movieInfoCopy)
+  };
+
+}
